1d0087de49c27f536702aceaad081eb51493b543
c19719949 <57361393+c19719949@users.noreply.github.com>
2022-10-10 10:31:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ addbutton = document.querySelector("#submit");
 n_name = document.querySelector("input");
 n_content = document.querySelector("textarea");
 display = document.querySelector("#displaysection");
+// deleteBtn = document.querySelector("#deleteButton");
+colourbtng = document.querySelector("#greenButton");
+colourbtnp = document.querySelector("#purpleButton");
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 show_all_notes();
-
 addbutton.addEventListener("click", () => {
 
     let note_name = n_name.value,
@@ -24,21 +26,32 @@ addbutton.addEventListener("click", () => {
         localStorage.setItem("notes", JSON.stringify(notes));
         
     }
+    show_all_notes();
     
 
 });
 
 
-function show_all_notes(){
+
+function show_all_notes()
+{
     document.querySelectorAll(".note").forEach(note => note.remove());
-    notes.forEach((note) => {
+    notes.forEach((note, index) => {
 
                 let list = `<li class = "note">
                 <p>${note.note_title}</p>
                 <span>${note.contents}</span>
+                <button id = "deleteButton">Delete Note</button>
+                <button id = "greenButton">Set Colour To Green</button>
+                <button id = "purpleButton">Set Colour To Purple</button>
         
         `;
 
         addnote.insertAdjacentHTML("afterend", list);
     });
+}
+
+function deleteNote()
+{
+
 }
