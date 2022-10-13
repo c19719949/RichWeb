a8329181ba37afc8e0ec9a3e7c5d575bd8c8b8a6
c19719949 <57361393+c19719949@users.noreply.github.com>
2022-10-13 19:44:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const addnote = document.querySelector(".create_section");
+rectangles = document.querySelector(".note");
 textspace = document.querySelector("#input");
 addbutton = document.querySelector("#submit");
 n_name = document.querySelector("input");
 n_content = document.querySelector("textarea");
 display = document.querySelector("#displaysection");
-// deleteBtn = document.querySelector("#deleteButton");
 colourbtng = document.querySelector("#greenButton");
 colourbtnp = document.querySelector("#purpleButton");
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
@@ -35,23 +35,53 @@ addbutton.addEventListener("click", () => {
 
 function show_all_notes()
 {
+    if(!notes) return;
     document.querySelectorAll(".note").forEach(note => note.remove());
     notes.forEach((note, index) => {
 
                 let list = `<li class = "note">
                 <p>${note.note_title}</p>
-                <span>${note.contents}</span>
+                
+                <textarea>${note.contents}</textarea>
                 <button id = "deleteButton">Delete Note</button>
                 <button id = "greenButton">Set Colour To Green</button>
                 <button id = "purpleButton">Set Colour To Purple</button>
         
         `;
-
+        //console.log("index" + index);
         addnote.insertAdjacentHTML("afterend", list);
+        addDelete(index);
+      //  addcolours();
+        
+        
     });
+    
+
+}
+function addDelete(index){
+   // console.log("secondIndex"+index);
+   
+        //console.log("thirdIndex"+index);
+        deleteBtn = document.querySelector("#deleteButton");
+        deleteBtn.addEventListener("click", () => {
+        //console.log("here"+index);
+        //console.log(note.note_title);
+        deleteNote(index);
+    });
+    
 }
 
-function deleteNote()
+function deleteNote(ID)
 {
-
+    //console.log(ID);
+    notes.splice(ID, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    show_all_notes();
 }
+
+// function addcolours()
+// {
+//    colourbtng.addEventListener("click", () => {
+//         rectangles.style.backgroundcolor = 'green';
+//    });
+// }
